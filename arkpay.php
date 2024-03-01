2d51ba3231be2fe35bf3a60978816eb65a9db172
stefan <stefan@exn.rs>
2024-03-01 08:50:31 +0100

    plugin version

--- arkpay.php
+++ arkpay.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Arkpay
  * Plugin URI:        https://arkpay.com
  * Description:       The Smartest, Fastest & Most Secure Payment Processor.
- * Version:           1.0.5
+ * Version:           1.0.6
  * Author:            Arkpay
  * Author URI:        https://arkpay.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ARKPAY_VERSION', '1.0.5' );
+define( 'ARKPAY_VERSION', '1.0.6' );
 
 /**
  * The code that runs during plugin activation.
